88f8c8e69879f35f3167c1ed3d1db6149d93b8d2
Bskasan <b.kasan@yandex.com>
2023-12-26 20:29:30 +0200

    character card done - with image bugs

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 
-const CharacterCard = () => {
-  return <div>CharacterCard</div>;
+const CharacterCard = ({ name, img, info }) => {
+  const [showImg, setShowImg] = useState(true);
+
+  const handleClick = () => {
+    setShowImg(!showImg);
+  };
+
+  return (
+    <Card
+      onClick={handleClick}
+      className="rounded-2 m-auto player-card"
+      role="button"
+    >
+      {showImg ? (
+        <Card.Img className="player-img" variant="top" src={img} />
+      ) : (
+        <div className="m-auto p-4">
+          <p>{info}</p>
+        </div>
+      )}
+    </Card>
+  );
 };
 
 export default CharacterCard;
